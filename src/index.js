79167ec237731d0e93cd4467f2af82a743e7cea0
quannt <simpleplan.vn@gmail.com>
2021-01-03 09:36:28 +0700

    Add call to fetch issues

---
+++ src/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const { Octokit } = require("@octokit/rest")
+const octokit = new Octokit({
+  auth: process.env.AUTH,
+  userAgent: process.env.USER_AGENT
+})
+
+async function fetchData () {
+  const labels = (process.env.REPO_LABELS && process.env.REPO_LABELS.split(',').map(x => x.trim())) || []
+  return octokit
+    .paginate(octokit.issues.listForRepo, {
+      owner: process.env.REPO_OWNER,
+      repo: process.env.REPO_NAME,
+      labels,
+      state: 'open'
+    })
+    .then((issues) => {
+      // issues is an array of all issue objects
+      console.log(issues)
+    });
+}
+
+function generateSites (data) {
+  console.log(data)
+}
+
+
+fetchData().then((data) => {
+  generateSites(data)
+}).catch((error) => {
+  console.error(error)
+})
